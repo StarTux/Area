f0c1e6da7563f541a55b453eac64123bb8f11fab
StarTux <ths.loy@googlemail.com>
2021-10-13 19:30:55 +0200

    More helpers

--- src/main/java/com/cavetale/area/struct/Vec3i.java
+++ src/main/java/com/cavetale/area/struct/Vec3i.java
@@ -4,6 +4,7 @@ import lombok.Value;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 
 @Value
 public final class Vec3i {
@@ -17,6 +18,10 @@ public final class Vec3i {
         return new Vec3i(block.getX(), block.getY(), block.getZ());
     }
 
+    public static Vec3i of(Location location) {
+        return new Vec3i(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
     public Block toBlock(World world) {
         return world.getBlockAt(x, y, z);
     }
@@ -25,8 +30,28 @@ public final class Vec3i {
         return toBlock(world).getLocation().add(0.5, 0.0, 0.5);
     }
 
+    public BlockFace horizontalBlockFace() {
+        if (Math.abs(x) > Math.abs(z)) {
+            return x > 0 ? BlockFace.EAST : BlockFace.WEST;
+        } else {
+            return z > 0 ? BlockFace.SOUTH : BlockFace.NORTH;
+        }
+    }
+
     public Vec3i add(int dx, int dy, int dz) {
         return new Vec3i(x + dx, y + dy, z + dz);
+    }
+
+    public Vec3i add(Vec3i other) {
+        return new Vec3i(x + other.x, y + other.y, z + other.z);
+    }
+
+    public Vec3i subtract(int dx, int dy, int dz) {
+        return new Vec3i(x - dx, y - dy, z - dz);
+    }
+
+    public Vec3i subtract(Vec3i other) {
+        return new Vec3i(x - other.x, y - other.y, z - other.z);
     }
 
     public int distanceSquared(Vec3i other) {
